8bff6b3b5d1c85cc1cd3833dcb8d53d22febf50a
Prashna <dhakalph@mail.uc.edu>
2025-09-19 15:12:43 -0400

    Completed task 4

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num=3;
+        System.out.println("The given number is: " + num);
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number.");
+        } else {
+            System.out.println(num + " is an odd number.");
+        }
+    }
+}
